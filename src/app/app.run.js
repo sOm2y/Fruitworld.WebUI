3c632d2ff3504fef1902949841d9aa648b4f3ade
sOm2y <yyin888@aucklanduni.ac.nz>
2016-09-10 23:22:18 +1200

    add state detect

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -6,7 +6,13 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log,$rootScope) {
+		$rootScope.$on('$stateChangeSuccess', function(ev, to, toParams, from, fromParams) {
+		    $rootScope.previousState = from.name;
+		    $rootScope.currentState = to.name;
+		    console.log('Previous state:'+$rootScope.previousState);
+		    console.log('Current state:'+$rootScope.currentState);
+		});
 
     $log.debug('runBlock end');
   }
