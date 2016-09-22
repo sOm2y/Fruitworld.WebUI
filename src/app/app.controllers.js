fbbf40c6482e7cb09e09f1ceacd68b3594891472
Hao Zhang <klaushaozhang@gmail.com>
2016-09-22 22:25:46 +1200

    add $log information

--- src/app/app.controllers.js
+++ src/app/app.controllers.js
@@ -4,13 +4,17 @@
   angular.module('fruitWorld')
     .controller('appCtrl', [
       '$scope',
+      '$log',
       'tokenManger',
       function ($scope,
+                $log,
                 tokenManager) {
 
         //is this the main controller?
+        $log.debug('token manager initializing.');
         var mgr = tokenManager.getTokenManager();
         if (mgr.expired) {
+          $log.debug('token expired, redirecting for new token.')
           // TODO: uncomment this for authentication
           //mgr.redirectForToken();
         }
