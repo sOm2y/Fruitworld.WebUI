dc644c72b1b674dcf3187f0fbf5290963c534a83
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-12 23:24:21 +1300

    create shoppingcart controller and fill in variables to template

---
+++ src/app/shoppingCart/shoppingCart.controllers.js
@@ -0,0 +1,8 @@
+(function(){
+  'use strict';
+  angular.module('fruitWorld')
+    .controller('shoppingCartCtrl', ['$scope','shoppingCartService',function ($scope,shoppingCartService) {
+      $scope.cartProducts = shoppingCartService.getShoppingCart();
+      $scope.countedShoppingCart = shoppingCartService.countDuplicateProducts($scope.cartProducts);
+    }]);
+})();
