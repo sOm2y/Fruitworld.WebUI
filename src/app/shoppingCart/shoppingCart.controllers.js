6a9a0470897e4e94c8a626e325273aa60d7c9408
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-15 21:25:48 +1300

    shopping cart template and chontroller

--- src/app/shoppingCart/shoppingCart.controllers.js
+++ src/app/shoppingCart/shoppingCart.controllers.js
@@ -4,5 +4,8 @@
     .controller('shoppingCartCtrl', ['$scope','shoppingCartService',function ($scope,shoppingCartService) {
       $scope.cartProducts = shoppingCartService.getShoppingCart();
       $scope.countedShoppingCart = shoppingCartService.countDuplicateProducts($scope.cartProducts);
+      $scope.addToBox = function(products){
+        //TODO
+      };
     }]);
 })();
