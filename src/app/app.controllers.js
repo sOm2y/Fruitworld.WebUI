420e738abbd93f8ed8391df31789cdaaf766e9ee
sOm2y <yyin888@aucklanduni.ac.nz>
2016-10-15 21:24:42 +1300

    add watch to main controller to watch shopping cart

--- src/app/app.controllers.js
+++ src/app/app.controllers.js
@@ -2,15 +2,30 @@
   'use strict';
 
   angular.module('fruitWorld')
-    .controller('appCtrl', ['$scope',function ($scope) {
+    .controller('appCtrl', ['$rootScope','$scope','shoppingCartService',function ($rootScope,$scope,shoppingCartService) {
       //watch shopping cart
       $scope.$watch(function () {
         return localStorage.getItem('shoppingCart');
       },function(newVal,oldVal){
         if(localStorage.getItem('shoppingCart')){
           $scope.selectedProducts = JSON.parse(localStorage.getItem('shoppingCart'));
+          $scope.countedProducts = shoppingCartService.countDuplicateProducts($scope.selectedProducts);
+          $scope.totalProducts = _.sumBy($scope.countedProducts , function(product) { return product.count; });
         }
-      });
-      
+      }, true);
+      // applySavedShoppingCart();
+      //
+      // $rootScope.$on('broadcastShoppingCart', function (event, data) {
+      //   applySavedShoppingCart();
+      // });
+      //
+      // function applySavedShoppingCart(){
+      //   if(localStorage.getItem('shoppingCart')){
+      //     $scope.selectedProducts = JSON.parse(localStorage.getItem('shoppingCart'));
+      //     $scope.countedProducts = shoppingCartService.countDuplicateProducts($scope.selectedProducts);
+      //     $scope.totalProducts = _.sumBy($scope.countedProducts , function(product) { return product.count; });
+      //   }
+      // }
+
     }]);
 })();
